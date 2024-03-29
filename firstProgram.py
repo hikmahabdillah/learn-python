512502380ddc3270e2c3d921ea61b3eb9fda4a6e
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-03-29 22:49:23 +0700

    learn how to use | input, output & comment

--- firstProgram.py
+++ firstProgram.py
@@ -5,3 +5,14 @@ print ("Hello World")
 addition = 2+2
 result = addition + 1
 print(result)
+
+# input and output 
+name = input("Masukkan Nama Anda : ")
+print('Nama : ' + name)
+
+# comment in python
+"""
+Output:
+Masukkan Nama Anda : Hikmah
+Nama : Hikmah
+"""
