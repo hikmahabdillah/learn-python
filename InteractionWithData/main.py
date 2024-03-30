e9fd909598020f8b83d2a43ecf00f0ef565a38f7
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-03-30 09:27:30 +0700

    learn how to use | boolean

--- InteractionWithData/main.py
+++ InteractionWithData/main.py
@@ -6,6 +6,20 @@ y = '6'
 print(type(y))
 # <class 'str'>
 
-x = float
+x = 6.0
 print(type(x))
 # <class 'float'>
+
+# BOOLEAN
+#  Boolean data type is used to represent the truth value
+z = True
+print(type(z))
+
+z = False
+print(type(z))
+
+"""
+  Output : 
+  <class 'bool'>
+  <class 'bool'>
+"""
