094d6893240c896a5e5e1c24953aeed428cc02b1
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-01 06:20:07 +0700

    learn how to use | in & not in, provide value to multi var, and sorting

--- InteractionWithData/listSetStringOperation.py
+++ InteractionWithData/listSetStringOperation.py
@@ -30,3 +30,37 @@ print(even.count(6))  #how many times the number 6 appears in the list even
 # on string
 text = "hello world"
 print(text.count("o")) #how many times the letter o appears in the text
+
+
+# IN & NOT IN
+sentence = "Learn Python with Particle"
+print('Particle' in sentence)
+print('Nothing' in sentence)
+
+# PROVIDING VALUE TO MULTIPLE VARIABLE
+data = ['shirt', 'white', 'L']
+"""
+apparel = data[0]
+color = data[1]
+size = data[2]
+"""
+# or
+apparel, color, size = data
+print(data)
+print(apparel)
+print(color)
+print(size)
+
+# SORTING
+vehicle = ['motorcycle', 'plane', 'helicopter', 'car']
+vehicle.sort()  # ascending
+print(vehicle)
+
+vehicle.sort(reverse=True)  # descending
+print(vehicle)
+
+# SORT CAN ONLY BE HANDLED WITH ONE DATA TYPES
+# the value of uppercase letters will come before lowercase letters.
+word = ["Zebra", "apple", "Orange", "banana"]
+word.sort()
+print(word)
