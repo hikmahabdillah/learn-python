17f6823f7391ca2dce4f3b363b8314fbafab646d
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-01 06:07:09 +0700

    learn how to use | len, min max, count

---
+++ InteractionWithData/listSetStringOperation.py
@@ -0,0 +1,32 @@
+# LENGTH
+# on list
+list = [1,3,2,4,5,3,3,8]
+
+print(list)
+print(len(list))
+
+# on set
+sets = set([1,2,2,3,4,2,3,5])
+
+print(sets)
+print(len(sets))
+
+# on string
+string = "hello world"
+
+print(string)
+print(len(string))
+
+# MIN MAX
+number = [33,2,44,24,21,46,99,32,5,3]
+
+print(min(number))
+print(max(number))
+
+# COUNT
+even = [2,2,4,4,4,6,6,6,6,8,8,8,10,10]
+print(even.count(6))  #how many times the number 6 appears in the list even
+
+# on string
+text = "hello world"
+print(text.count("o")) #how many times the letter o appears in the text
