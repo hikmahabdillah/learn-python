628fd4a1644c6fa43f62ac18bac3b2e121b88d79
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-09 10:04:09 +0700

    learn how to use | introduction of sequential action

---
+++ SequntialAction/introduction.py
@@ -0,0 +1,13 @@
+print("Welcome to Python Program")
+print("Input your Data")
+name = input("Enter Your Name : ")
+year_of_birth = input("Enter Your Year of Birth : ")
+current_age = 2024 - int(year_of_birth)
+
+print(f"Welcome, {name}. Your Currently Age is {current_age} Years Old.")
+
+
+a = 4
+b = 6
+print(a**2)
+print(b//2)
