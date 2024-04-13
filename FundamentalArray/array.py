a65b90e1f1c8386f19031fc84ec6bc841b9eb50f
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-04-13 20:20:51 +0700

    learn how to use | get max value of array use divide conquer concept

--- FundamentalArray/array.py
+++ FundamentalArray/array.py
@@ -26,3 +26,14 @@ for i in range(len(var_arr)):
         next_element = None
         
     print(f"Current element: {current_element}, next elements: {next_element}")
+
+# GET MAX VALUE OF ARRAY
+var_arr1 = [1, 7, 2, 89, 3]
+left_pointer = var_arr1[0]
+
+for i in range(1, len(var_arr1)):
+    right_pointer = var_arr1[i]
+    if right_pointer > left_pointer:
+        left_pointer = right_pointer
+
+print(left_pointer)
