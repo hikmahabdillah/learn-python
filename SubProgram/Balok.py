3fec26b5ce9144754eb1c74222a040e1954e5916
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-05-10 10:45:25 +0700

    learn how to implement module in py

---
+++ SubProgram/Balok.py
@@ -0,0 +1,4 @@
+import functionBalok
+
+balok_ketiga = functionBalok.countHitungVolumeBalok(4,5,6)
+print(balok_ketiga)
---
+++ SubProgram/functionBalok.py
@@ -0,0 +1,3 @@
+def countHitungVolumeBalok(panjang,lebar, tinggi):
+    volume_balok = panjang*lebar*tinggi
+    return volume_balok
