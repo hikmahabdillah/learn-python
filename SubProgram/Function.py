21ca883f04fb6ffe49a4dfa7ddf3559c52ca3839
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-05-10 09:45:34 +0700

    learn how to use function in py

---
+++ SubProgram/Function.py
@@ -0,0 +1,9 @@
+def countHitungVolumeBalok(panjang,lebar, tinggi):
+    volume_balok = panjang*lebar*tinggi
+    return volume_balok
+
+balok_pertama = countHitungVolumeBalok(5,10,4)
+print(balok_pertama)
+
+balok_kedua = countHitungVolumeBalok(4,15,8)
+print(balok_kedua)
