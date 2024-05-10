25c1be08d59d5a57d04541c47e45ad9d707d2232
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-05-10 10:32:19 +0700

    learn how to use function in py | with various of parameter

--- SubProgram/Function.py
+++ SubProgram/Function.py
@@ -7,3 +7,27 @@ print(balok_pertama)
 
 balok_kedua = countHitungVolumeBalok(4,15,8)
 print(balok_kedua)
+
+# Positional-or-Keyword
+def greeting(name, message):
+    return "Halo " + name + "! ," + message
+
+print(greeting("Aldrin", "Keep Going!"))
+print(greeting(message="Keep Smile! ", name="Abdillah"))
+
+# Var-Positional (Variadic Positional Parameter)
+def hitung_total(*args):
+    print(type(args))
+    total = sum(args)
+    return total
+
+print(hitung_total(4, 2, 1, 3))
+
+# Var-Keyword (Variadic Keyword Parameter)
+def cetak_info(**kwargs):
+    info = ""
+    for key, value in kwargs.items():
+        info += key + ': ' + value + ", "
+    return info
+
+print(cetak_info(name="Hikmah Aldrin Abdillah", age="19", job="Front End Developer"))
