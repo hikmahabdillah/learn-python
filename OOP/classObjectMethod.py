62b309f4c462ae1536cf71b64c6a366029777e18
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-05-17 21:42:05 +0700

    Added Mobil class with object method tambah_kecepatan and created an instance mobil_1

--- OOP/classObjectMethod.py
+++ OOP/classObjectMethod.py
@@ -26,3 +26,24 @@ class Student:
 
 student4 = Student("Abdillah", "TI-1F")
 print(student4.name, student4.classs)
+
+# METHOD
+
+# object method
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+
+    def tambah_kecepatan(self):
+        self.kecepatan += 10
+
+mobil_1 = Mobil("Merah", "DicodingCar", 160)
+print("Sebelum ditambahkan: ")
+print(mobil_1.kecepatan)
+
+mobil_1.tambah_kecepatan()        # Memanggil metode tambah_kecepatan.
+
+print("Setelah ditambahkan: ")
+print(mobil_1.kecepatan)
