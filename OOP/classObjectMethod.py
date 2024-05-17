eb5db94bf14fb6bbb82e20e46faf3b13d3b4f0aa
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-05-17 21:06:48 +0700

    learn start of oop in python

---
+++ OOP/classObjectMethod.py
@@ -0,0 +1,11 @@
+class Student:
+  name = "Hikmah"
+  classs = "TI-1F"
+
+student1 = Student()
+print(student1.name)
+print(student1.classs)
+
+student2 = Student()
+student2.name = "Aldrin"
+print(student2.name, student2.classs)
