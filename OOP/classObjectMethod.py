c832769d24ce9b493923ba3e38478e690c0d4233
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-05-17 21:26:08 +0700

    learn how to make default constructor in python

--- OOP/classObjectMethod.py
+++ OOP/classObjectMethod.py
@@ -1,11 +1,20 @@
 class Student:
-  name = "Hikmah"
-  classs = "TI-1F"
+  # name = "Hikmah"
+  # classs = "TI-1F"
+
+  # default constructor
+  def __init__(self):
+    self.name = 'Hikmah'
+    self.classs = "TI-1F"
 
 student1 = Student()
-print(student1.name)
-print(student1.classs)
+print(student1.name, student1.classs)
 
 student2 = Student()
 student2.name = "Aldrin"
 print(student2.name, student2.classs)
+
+student3 = Student()
+student3.name = "Eric"
+student3.classs = "TI-1D"
+print(student3.name, student3.classs)
